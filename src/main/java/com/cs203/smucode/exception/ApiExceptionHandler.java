e641154b45aeb112ea63e2dd6122f65641006769
jeremainecheong <132808191+jeremainecheong@users.noreply.github.com>
2024-09-06 23:58:17 +0800

    Add basic custom exception

---
+++ src/main/java/com/cs203/smucode/exception/ApiExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.cs203.smucode.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.ZonedDateTime;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+    @ExceptionHandler(value = {ApiRequestException.class})
+    public ResponseEntity<ApiException> handleApiRequestException(ApiRequestException e) {
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        ApiException apiException = new ApiException(
+                e.getMessage(),
+                badRequest,
+                ZonedDateTime.now()
+        );
+        return new ResponseEntity<>(apiException, badRequest);
+    }
+}
