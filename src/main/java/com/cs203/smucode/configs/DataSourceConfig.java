2f5f0a234d1dbb56f70428a0d6dff01a5b768429
gavwyh <wongyhpersonal@gmail.com>
2024-09-06 21:38:24 +0800

    Add AWS support for RDS & secret manager

---
+++ src/main/java/com/cs203/smucode/configs/DataSourceConfig.java
@@ -0,0 +1,70 @@
+package com.cs203.smucode.configs;
+
+import com.cs203.smucode.utils.AWSUtils;
+
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.rds.RdsClient;
+import software.amazon.awssdk.services.rds.model.DBInstance;
+import software.amazon.awssdk.services.rds.model.DescribeDbInstancesRequest;
+import software.amazon.awssdk.services.rds.model.DescribeDbInstancesResponse;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    // AWS-related configuration properties
+    @Value("${aws.rds.instance.identifier}")
+    private String dbInstanceIdentifier; // Identifier for the RDS instance
+
+    @Value("${aws.region}")
+    private String awsRegion; // AWS region where the RDS instance is located
+
+    @Value("${spring.datasource.username}")
+    private String dbUsername; // Database username
+
+    @Value("${aws.secretsmanager.db.password.secret}")
+    private String dbPasswordSecretName; // Name of the secret in AWS Secrets Manager that stores the database password
+    
+    @Bean
+    public DataSource dataSource() {
+        // Create an RDS client to interact with AWS RDS service
+        RdsClient rdsClient = RdsClient.builder()
+                .credentialsProvider(DefaultCredentialsProvider.create()) // Use default AWS credentials provider chain
+                .region(Region.of(awsRegion)) // Set the AWS region
+                .build();
+
+        // Create a request to describe the RDS instance
+        DescribeDbInstancesRequest request = DescribeDbInstancesRequest.builder()
+                .dbInstanceIdentifier(dbInstanceIdentifier) // Specify which RDS instance to describe
+                .build();
+        
+        // Send the request and get the response
+        DescribeDbInstancesResponse response = rdsClient.describeDBInstances(request);
+        DBInstance dbInstance = response.dbInstances().get(0); // Get the first (and should be only) instance described
+
+        // Construct the JDBC URL using the RDS instance details
+        String jdbcUrl = String.format("jdbc:postgresql://%s:%d/%s",
+                dbInstance.endpoint().address(), // RDS instance hostname
+                dbInstance.endpoint().port(), // RDS instance port
+                dbInstance.dbName()); // Database name
+
+        // Retrieve the database password from AWS Secrets Manager
+        String dbPassword = AWSUtils.getValueFromSecretsManager(dbPasswordSecretName);
+
+        // Configure HikariCP connection pool
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(jdbcUrl);
+        config.setUsername(dbUsername);
+        config.setPassword(dbPassword);
+        config.setDriverClassName("org.postgresql.Driver"); 
+        
+        // Create and return a HikariDataSource with the configured properties; repository will use this to connect to the database
+        return new HikariDataSource(config);
+    }
+}
---
+++ src/main/java/com/cs203/smucode/utils/AWSUtils.java
@@ -0,0 +1,44 @@
+package com.cs203.smucode.utils;
+
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+import software.amazon.awssdk.core.exception.SdkException;
+
+/**
+ * @author: gav
+ * @version: 1.0
+ * @since: 24-09-06
+ * @description: Utility class for AWS services
+ */
+@Component
+public class AWSUtils {
+    
+    public static String getValueFromSecretsManager(String secretName) {
+        SecretsManagerClient client = SecretsManagerClient.builder()
+                .region(Region.AP_SOUTHEAST_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+
+        GetSecretValueRequest secretValueRequest = GetSecretValueRequest.builder()
+                .secretId(secretName)
+                .build();
+        
+        GetSecretValueResponse secretValueResponse = null;
+
+        try {
+            secretValueResponse = client.getSecretValue(secretValueRequest);
+        } catch (SdkException e) {
+            e.printStackTrace(); // TODO: handle exception -> add logging
+        }
+        
+        if (secretValueResponse == null) {
+            return null;
+        }
+        
+        return secretValueResponse.secretString();
+    }
+}
