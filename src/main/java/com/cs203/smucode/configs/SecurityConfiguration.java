ed06b8005f8efade7b767b885826ecf0867d44ca
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-09-06 18:36:41 +0800

    Add bcrypt hashing

---
+++ src/main/java/com/cs203/smucode/configs/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.cs203.smucode.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author: gav
+ * @version: 1.0
+ * @since: 2024-09-05
+ */
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Used for encoding passwords upon user creation
+    }
+
+    
+}
