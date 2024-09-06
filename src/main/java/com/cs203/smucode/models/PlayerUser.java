d8c77db50d7047cf19e7f98f218371431db7f98b
gavwyh <wongyhpersonal@gmail.com>
2024-09-06 18:46:11 +0800

    Add basic player decorator for user

---
+++ src/main/java/com/cs203/smucode/models/PlayerUser.java
@@ -0,0 +1,30 @@
+package com.cs203.smucode.models;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class PlayerUser implements UserDetails {
+    private final User user;
+
+    public PlayerUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> user.getAuthority());
+    }
+    
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
