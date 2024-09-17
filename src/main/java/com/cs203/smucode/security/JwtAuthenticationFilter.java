6da02bbd0e931cda5a2468d565a3db175ff35153
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-09-17 11:51:54 +0800

    Add rough jwt implementation

---
+++ src/main/java/com/cs203/smucode/security/JwtAuthenticationFilter.java
@@ -0,0 +1,19 @@
+package com.cs203.smucode.security;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+}
--- src/main/java/com/cs203/smucode/utils/JWTUtil.java
+++ src/main/java/com/cs203/smucode/utils/JWTUtil.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 * @author: gav
 * @version: 1.0
 * @since: 24-09-06
-* @description: Utility class for JWT operations
+* @description: Utility class for JWT operations (generate, validate, get subject)
 */
 @Component
 public class JWTUtil {
@@ -24,4 +24,28 @@ public class JWTUtil {
    @Value("${jwt.expiration}")
    private long expiration; // 1 hour
 
+    public String generateToken(String username) { 
+        // Convert the secret key to a Key object -> cuz need key to cryptographically sign the JWT
+        Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+        
+        // Build the JWT 
+        return Jwts.builder()
+            .subject(username)
+            .issuedAt(new Date(System.currentTimeMillis()))
+            .expiration(new Date(System.currentTimeMillis() + expiration))
+            .signWith(key) 
+            .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(jwtSecret.getBytes()).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    
+    
+    public String getUsernameFromToken(String token) {
 }
