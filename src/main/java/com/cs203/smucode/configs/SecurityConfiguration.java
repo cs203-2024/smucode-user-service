d19c649bbe25e6c25a9c564ff01e7947bf1a58ce
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-09-23 16:40:22 +0800

    Add local dev support

--- src/main/java/com/cs203/smucode/configs/SecurityConfiguration.java
+++ src/main/java/com/cs203/smucode/configs/SecurityConfiguration.java
@@ -1,12 +1,20 @@
 package com.cs203.smucode.configs;
 
+import com.cs203.smucode.utils.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer.JwtConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * @author: gav
@@ -14,18 +22,23 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * @since: 2024-09-05
  */
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(); // Used for encoding passwords upon user creation
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-            auth -> auth.anyRequest().permitAll()
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+        throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        http.sessionManagement(session ->
+            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
+        http.csrf().disable();
         return http.build();
     }
-
 }
