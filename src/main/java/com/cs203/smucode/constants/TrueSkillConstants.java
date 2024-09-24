65c3b51dce88232be785c7aaee497c3724915c73
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-09-24 22:02:14 +0800

    Refactor TrueSkillConstant

--- src/main/java/com/cs203/smucode/constants/TrueSkillConstants.java
+++ src/main/java/com/cs203/smucode/constants/TrueSkillConstants.java
@@ -1,7 +1,7 @@
 package com.cs203.smucode.constants;
 
-public class TrueSkillConstants {
-    public static final double DEFAULT_MU = 25.0;
-    public static final double DEFAULT_SIGMA = 8.333; //default standard deviation
-    public static final int K_FACTOR = 3; //"confidence" parameter
+public interface TrueSkillConstants {
+    double DEFAULT_MU = 25.0;
+    double DEFAULT_SIGMA = 8.333; //default standard deviation
+    int K_FACTOR = 3; //"confidence" parameter
 }
