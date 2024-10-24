6b2bd8acccdb4af737aa95f343ae63af1262b728
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-10-24 21:18:43 +0800

    Add endpoint to create profile

--- src/main/java/com/cs203/smucode/controllers/UserController.java
+++ src/main/java/com/cs203/smucode/controllers/UserController.java
@@ -1,19 +1,22 @@
 package com.cs203.smucode.controllers;
 
 import com.cs203.smucode.exception.ApiRequestException;
-import com.cs203.smucode.mappers.UserMapper;
-import com.cs203.smucode.models.User;
+import com.cs203.smucode.mappers.UserProfileMapper;
+import com.cs203.smucode.models.UserIdentificationDTO;
 import com.cs203.smucode.models.UserInfoDTO;
+import com.cs203.smucode.models.UserProfile;
+import com.cs203.smucode.models.UserRatingDTO;
 import com.cs203.smucode.services.IUserService;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
-
-import java.util.Map;
+import java.net.URI;
 import de.gesundkrank.jskills.Rating;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  * @author: jere
@@ -33,60 +36,63 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/{username}")
-    public ResponseEntity<UserInfoDTO> getUser(@PathVariable String username) {
+
+    @GetMapping("/profile/{username}")
+    public ResponseEntity<UserInfoDTO> getProfile(@PathVariable String username) {
         try {
             validateUsername(username);
 
-            User user = userService.getUserByUsername(username);
-            UserInfoDTO userInfoDTO = UserMapper.INSTANCE.userToUserDTO(user);
+            UserProfile user = userService.getUserProfileByUsername(username);
+            UserInfoDTO userInfoDTO = UserProfileMapper.INSTANCE.userProfileToUserInfoDTO(user);
 
             return ResponseEntity.ok(userInfoDTO);
         } catch (ApiRequestException e) {
             throw e;
         } catch (Exception e) {
-            throw new ApiRequestException("An error occurred while fetching the user", e);
+            throw new ApiRequestException("An error occurred while fetching the user profile");
         }
     }
 
-    @GetMapping("/profile")
-    public ResponseEntity<UserInfoDTO> getProfile(@RequestParam String username) {
+    @PostMapping("/profile/create")
+    public ResponseEntity<String> createUser(@RequestBody UserIdentificationDTO userIdentificationDTO) {
         try {
-            validateUsername(username);
-            User user = userService.getUserByUsername(username);
-            UserInfoDTO userInfoDTO = UserMapper.INSTANCE.userToUserDTO(user);
-            return ResponseEntity.ok(userInfoDTO);
+            validateUsername(userIdentificationDTO.username());
+
+            UserProfile userProfile = UserProfileMapper.INSTANCE.userIdentificationDTOtoUserProfile(userIdentificationDTO);
+
+            userService.createUserProfile(userProfile);
+
+            URI location = ServletUriComponentsBuilder
+                    .fromCurrentContextPath()         // Start from the base URL
+                    .path("/api/users/profile")       // Add the base path including "profile"
+                    .path("/{username}")              // Append the username parameter
+                    .buildAndExpand(userProfile.getUsername())  // Insert the username into the path
+                    .toUri();
+
+            return ResponseEntity.created(location)
+                    .body("Created user profile '" + userProfile + "'.");
         } catch (ApiRequestException e) {
             throw e;
         } catch (Exception e) {
-            throw new ApiRequestException("An error occurred while fetching the user profile", e);
+            throw new ApiRequestException("An error occurred while fetching the user profile");
         }
     }
 
     //true skill-related
-    @PutMapping("/{username}/update-rating")
-    public ResponseEntity<String> updateRating(@PathVariable String username, @RequestBody Map<String, Object> ratingData) {
+    @PutMapping("/update-rating")
+    public ResponseEntity<String> updateRating(@RequestBody @Valid UserRatingDTO ratingDTO) {
         try {
-            //validate that both 'mu' and 'sigma' are present, and of valid type
-            if (!ratingData.containsKey("mu") || !(ratingData.get("mu") instanceof Number)) {
-                throw new ApiRequestException("'mu' is required and must be a valid number");
-            }
-            if (!ratingData.containsKey("sigma") || !(ratingData.get("sigma") instanceof Number)) {
-                throw new ApiRequestException("'sigma' is required and must be a valid number");
-            }
+            validateUsername(ratingDTO.username());
 
-            //then we cast to double
-            double mu = ((Number) ratingData.get("mu")).doubleValue();
-            double sigma = ((Number) ratingData.get("sigma")).doubleValue();
-
-            Rating newRating = new Rating(mu, sigma);
-            userService.updateUserRating(username, newRating);
+            Rating newRating = new Rating(ratingDTO.mu(), ratingDTO.sigma());
+            userService.updateUserRating(ratingDTO.username(), newRating);
 
             return ResponseEntity.ok("User rating updated successfully");
         } catch (ApiRequestException e) {
             throw e;
         } catch (Exception e) {
-            throw new ApiRequestException("An error occurred while updating the user's rating", e);
+            logger.error("error updating rating", e);
+            throw new ApiRequestException("An error occurred while updating the user's rating");
         }
     }
 
