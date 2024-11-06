ada2ad8bb8b1f837bae8457f4396ea4360eb05ff
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-11-06 13:18:21 +0800

    chore: fix severe lowi q

--- src/main/java/com/cs203/smucode/constants/MediaConstants.java
+++ src/main/java/com/cs203/smucode/constants/MediaConstants.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 public final class MediaConstants {
 
-    public static final List<String> SUPPORTED_MEDIA
-            = List.of("image/png", "image/jpeg", "image/gif");
     public static final String IMAGE_PNG = "image/png";
     public static final String IMAGE_JPEG = "image/jpeg";
     public static final String IMAGE_GIF = "image/gif";
+    public static final List<String> SUPPORTED_MEDIA
+            = List.of(IMAGE_PNG, IMAGE_JPEG, IMAGE_GIF);
 
     private MediaConstants() {
         throw new IllegalStateException("Constant class");
